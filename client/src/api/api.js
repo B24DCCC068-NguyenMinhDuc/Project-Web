a95ff328b2cc9dc172b5882b8bee520d620c1342
B24DCCC068_NguyenMinhDuc <DucNM.B24CC068@stu.ptit.edu.vn>
2025-12-04 10:04:40 +0700

    remove node_modules

---
+++ client/src/api/api.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+const api = axios.create({ baseURL: 'http://localhost:5000/api' });
+
+export const loginAPI = (data) => api.post('/login', data);
+export const registerAPI = (data) => api.post('/register', data); // Mới
+export const getDataAPI = () => api.get('/data');
+export const updateAppointmentAPI = (id, data) => api.put(`/appointments/${id}`, data);
+export const addAppointmentAPI = (data) => api.post('/appointments', data);
+export const addDoctorAPI = (data) => api.post('/doctors', data);
+export const addPatientAPI = (data) => api.post('/patients', data);
+export const updatePatientAPI = (id, data) => api.put(`/patients/${id}`, data);
+export const deletePatientAPI = (id) => api.delete(`/patients/${id}`);
+
+export const registerScheduleAPI = (id, data) => api.post(`/doctors/${id}/schedule`, data);
+export const getDoctorAppointmentsAPI = (doctorId) => api.get(`/doctors/${doctorId}/appointments`);
+
+export const googleLoginAPI = (data) => api.post('/google-login', data);
+export default api;
